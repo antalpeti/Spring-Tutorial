94a35d480cf61d2899420885bd8794a139f88ec0
Peti <antalpeti@users.noreply.github.com>
2015-04-21 16:25:20 +0200

    Added example about usage of the prototype scope.

--- src/com/tutorialspoint/beanscopes/MainApp.java
+++ src/com/tutorialspoint/beanscopes/MainApp.java
@@ -7,12 +7,23 @@ public class MainApp {
   public static void main(String[] args) {
     ApplicationContext context = new ClassPathXmlApplicationContext("BeanScopesBeans.xml");
 
-    HelloWorld objA = (HelloWorld) context.getBean("singletonHelloWorld");
+    examineScope(context, "singletonHelloWorld");
+    examineScope(context, "prototypeHelloWorld");
+  }
+
+  /**
+   * Examine the behavior of the given scope.
+   *
+   * @param context
+   */
+  private static void examineScope(ApplicationContext context, String scope) {
+    System.out.println("---" + scope + "---");
+    HelloWorld objA = (HelloWorld) context.getBean(scope);
     objA.getMessage();
     objA.setMessage("I'm object A");
     objA.getMessage();
 
-    HelloWorld objB = (HelloWorld) context.getBean("singletonHelloWorld");
+    HelloWorld objB = (HelloWorld) context.getBean(scope);
     objB.getMessage();
   }
 }
