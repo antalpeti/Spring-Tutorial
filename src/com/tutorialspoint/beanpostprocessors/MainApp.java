79c8e6e63c0c9a71eea36de666eb8250c5e58773
Peti <antalpeti@users.noreply.github.com>
2015-04-24 01:13:28 +0200

    Added another bean to examine the behavior of the BeanPostProcessor.

--- src/com/tutorialspoint/beanpostprocessors/MainApp.java
+++ src/com/tutorialspoint/beanpostprocessors/MainApp.java
@@ -11,6 +11,10 @@ public class MainApp {
 
     HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
     obj.getMessage();
+    Person obj1 = (Person) context.getBean("person");
+    System.out.println("Your Name: " + obj1.getName());
+    System.out.println("Your Age: " + obj1.getAge());
+
     context.registerShutdownHook();
   }
 }
