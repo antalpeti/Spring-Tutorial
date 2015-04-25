4826db45a391e1bb3275176a8730b24e9ad68427
Peti <antalpeti@users.noreply.github.com>
2015-04-25 09:25:01 +0200

    Added example about the setter-based dependecy injection using property tag.

--- src/com/tutorialspoint/dependencyinjection/MainApp.java
+++ src/com/tutorialspoint/dependencyinjection/MainApp.java
@@ -7,7 +7,8 @@ public class MainApp {
   public static void main(String[] args) {
     ApplicationContext context = new ClassPathXmlApplicationContext("DependencyInjectionBeans.xml");
 
-    TextEditor te = (TextEditor) context.getBean("textEditor");
+    System.out.println("Constructor-Based:");
+    TextEditor te = (TextEditor) context.getBean("textEditorConstr");
 
     te.spellCheck();
 
@@ -15,5 +16,10 @@ public class MainApp {
     foo = (Foo) context.getBean("fooType");
     foo = (Foo) context.getBean("fooIndex");
     foo = (Foo) context.getBean("fooName");
+
+    System.out.println("Setter-Based:");
+    te = (TextEditor) context.getBean("textEditorSetter");
+
+    te.spellCheck();
   }
 }
