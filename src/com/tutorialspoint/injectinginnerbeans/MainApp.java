4753e867811120a8872fe4e980bab0b6dee75a45
Peti <antalpeti@users.noreply.github.com>
2015-04-26 17:19:55 +0200

    Added example about injecting inner bean inside a property element.

---
+++ src/com/tutorialspoint/injectinginnerbeans/MainApp.java
@@ -0,0 +1,14 @@
+package com.tutorialspoint.injectinginnerbeans;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+  public static void main(String[] args) {
+    ApplicationContext context = new ClassPathXmlApplicationContext("InjectingInnerBeansBeans.xml");
+
+    TextEditor te = (TextEditor) context.getBean("textEditor");
+
+    te.spellCheck();
+  }
+}
