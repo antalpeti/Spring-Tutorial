5a29513212fcda2a9a15b9b270eac662abc973a0
Peti <antalpeti@users.noreply.github.com>
2015-04-27 10:14:30 +0200

    Added example about injecting collection by passing direct values. Using value tag inside list and set tags. Using key and value attributes inside an entry tag in case of map tag. Using key attribute inside a prop tag in case of props tag.

---
+++ src/com/tutorialspoint/injectingcollection/MainApp.java
@@ -0,0 +1,17 @@
+package com.tutorialspoint.injectingcollection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+  public static void main(String[] args) {
+    ApplicationContext context = new ClassPathXmlApplicationContext("InjectingCollectionBeans.xml");
+
+    JavaCollection jc = (JavaCollection) context.getBean("javaCollection");
+
+    jc.getAddressList();
+    jc.getAddressSet();
+    jc.getAddressMap();
+    jc.getAddressProp();
+  }
+}
