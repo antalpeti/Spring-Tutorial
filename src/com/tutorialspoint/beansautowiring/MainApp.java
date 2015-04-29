d95c3090a69a39c301aa4b1cd3445bce7373565d
Peti <antalpeti@users.noreply.github.com>
2015-04-29 22:42:36 +0200

    Added example about autowiring 'byName'.

---
+++ src/com/tutorialspoint/beansautowiring/MainApp.java
@@ -0,0 +1,18 @@
+package com.tutorialspoint.beansautowiring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+  public static void main(String[] args) {
+    ApplicationContext context = new ClassPathXmlApplicationContext("BeansAutoWiring.xml");
+
+    TextEditor te = (TextEditor) context.getBean("textEditor");
+    te.spellCheck();
+    System.out.println(te.getName());
+
+    TextEditor teByName = (TextEditor) context.getBean("textEditorByName");
+    teByName.spellCheck();
+    System.out.println(teByName.getName());
+  }
+}
