04dde2711d85eb82ec159f09f590fa3abb69c939
Peti <antalpeti@users.noreply.github.com>
2015-04-30 12:23:12 +0200

    Added example about usage of @Required annotation.

---
+++ src/com/tutorialspoint/annotationbasedconfiguration/MainApp.java
@@ -0,0 +1,16 @@
+package com.tutorialspoint.annotationbasedconfiguration;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+  public static void main(String[] args) {
+    ApplicationContext context =
+        new ClassPathXmlApplicationContext("AnnotationBasedConfigurationBeans.xml");
+
+    Student student = (Student) context.getBean("student");
+
+    System.out.println("Name : " + student.getName());
+    System.out.println("Age : " + student.getAge());
+  }
+}
