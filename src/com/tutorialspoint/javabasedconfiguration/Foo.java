29b3473563d8dfc2c8f4c725afea30839c230647
Peti <antalpeti@users.noreply.github.com>
2015-05-02 18:23:52 +0200

    Added example about usage of @Bean annotation with arbitrary initialization and destruction callback methods.

---
+++ src/com/tutorialspoint/javabasedconfiguration/Foo.java
@@ -0,0 +1,11 @@
+package com.tutorialspoint.javabasedconfiguration;
+
+public class Foo {
+  public void init() {
+    System.out.println("Initialization steps.");
+  }
+
+  public void cleanup() {
+    System.out.println("Destruction steps.");
+  }
+}
